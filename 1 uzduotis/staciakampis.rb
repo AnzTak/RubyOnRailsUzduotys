98bf886fd6a2b4ad8a07cadcef35ffadec1d8683
AnzTak <anzelika.liut@gmail.com>
2021-01-11 00:20:32 +0200

    uzduotys versija 3

--- 1 uzduotis/staciakampis.rb
+++ 1 uzduotis/staciakampis.rb
@@ -1,71 +1,84 @@
+# frozen_string_literal: true
+
+# Rectangle
 class Rectangle
-	private
-	attr_accessor :ilgis, :plotis, :x, :y
-	
-	public
-	def initialize(ilgis, plotis, x, y)
-		@ilgis = ilgis
-		@plotis = plotis
-		@x = x
-		@y = y
-	end
+  private
 
-	def Plotas
-		return (@ilgis * @plotis).round()
-	end
+  attr_accessor :ilgis, :plotis, :x, :y
 
-	def Perimetras
-		return (2 * (@ilgis + @plotis)).round()
-	end
+  public
 
-	def Istrizaine
-		return Math.sqrt(@ilgis * @ilgis + @plotis * @plotis).round(2)
-	end
+  def initialize(ilgis, plotis, x_pos, y_pos)
+    @ilgis = ilgis
+    @plotis = plotis
+    @x = x_pos
+    @y = y_pos
+  end
 
-	def Kampai
-		bx = (@x + @ilgis).round()
-		by = (@y).round()
-		cx = (@x + @ilgis).round()
-		cy = (@y - @plotis).round()
-		dx = (@x).round()
-		dy = (@y - @plotis).round()
-		return "(#{@x.round()},#{@y.round()}) (#{bx},#{by}) (#{cx},#{cy}) (#{dx},#{dy})"
-	end
+  def plotas
+    (@ilgis * @plotis).round
+  end
 
-	def Centras
-		px = (@ilgis / 2) + @x
-		py = @y - (@plotis / 2)
-		return "(#{px.round()},#{py.round()})"
-	end
+  def perimetras
+    (2 * (@ilgis + @plotis)).round
+  end
+
+  def istrizaine
+    Math.sqrt(@ilgis * @ilgis + @plotis * @plotis).round(2)
+  end
+
+  def kampai
+    bx = (@x + @ilgis).round
+    by = @y.round
+    cx = (@x + @ilgis).round
+    cy = (@y - @plotis).round
+    dx = @x.round
+    dy = (@y - @plotis).round
+    "(#{@x.round},#{@y.round}) (#{bx},#{by}) (#{cx},#{cy}) (#{dx},#{dy})"
+  end
+
+  def centras
+    px = (@ilgis / 2) + @x
+    py = @y - (@plotis / 2)
+    "(#{px.round},#{py.round})"
+  end
 end
 
+# rubocop:disable Metrics/AbcSize
+
+# PrintRectangle
 class PrintRectangle
-	private
-	attr_accessor :ilgis, :plotis, :x, :y
-	
-	public
-	def Input
-		print "Iveskite staciakampio ilgi: "
-		@ilgis = gets.chomp
-		print "Iveskite staciakampio ploti: "
-		@plotis = gets.chomp
-		print "Iveskite kampo koordinate x: "
-		@x = gets.chomp
-		print "Iveskite kampo koordinate y: "
-		@y = gets.chomp
-		print "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n\n"
-	end
-	
-	def Print
-		r = Rectangle.new(ilgis.to_f, plotis.to_f, x.to_f, y.to_f)
-		puts "Staciakampio plotas lygus: #{r.Plotas}"
-		puts "Staciakampio perimetras lygus: #{r.Perimetras}"
-		puts "Staciakampio istrizaines ilgis lygus: #{r.Istrizaine}"
-		puts "Staciakampio kampu koordinates lygios: #{r.Kampai}"
-		puts "Staciakampio istrizainiu susikirtimo tasko koordinates lygios: #{r.Centras}"
-	end
+  private
+
+  attr_accessor :ilgis, :plotis, :x, :y
+
+  public
+
+  def data_input
+    print 'Iveskite staciakampio ilgi: '
+    @ilgis = gets.chomp
+    print 'Iveskite staciakampio ploti: '
+    @plotis = gets.chomp
+    print 'Iveskite kampo koordinate x: '
+    @x = gets.chomp
+    print 'Iveskite kampo koordinate y: '
+    @y = gets.chomp
+    print "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ \n\n"
+  end
+
+  def data_print
+    r = Rectangle.new(ilgis.to_f, plotis.to_f, x.to_f, y.to_f)
+    puts "Staciakampio plotas lygus: #{r.plotas}"
+    puts "Staciakampio perimetras lygus: #{r.perimetras}"
+    puts "Staciakampio istrizaines ilgis lygus: #{r.istrizaine}"
+    puts "Staciakampio kampu koordinates lygios: #{r.kampai}"
+    txt = 'Staciakampio istrizainiu susikirtimo'
+    puts "#{txt} tasko koordinates lygios: #{r.centras}"
+  end
 end
+
+# rubocop:enable Metrics/AbcSize
 
 rect = PrintRectangle.new
-rect.Input
-rect.Print
+rect.data_input
+rect.data_print
--- 2 uzduotis/pic.rb
+++ 2 uzduotis/pic.rb
@@ -1,53 +1,70 @@
+# frozen_string_literal: true
+
+# rubocop:disable Metrics/LineLength
+
+# rubocop:disable Metrics/MethodLength
+
+# rubocop:disable Metrics/AbcSize
+
+# Pic
 class Pic
-	private
-	attr_accessor :mano_vardas
-	
-	public
-	def initialize
-		@mano_vardas = []
-		for i in 0...128
-			@mano_vardas[i] = i.chr
-		end
-	end
+  private
 
-	def VardoSpausdinimas
-		arr = [65,110,122,101,108,105,107,97,32,84,97,107,117,110]
-		arr.each do |num|
-			print "#{@mano_vardas[num]}"
-		end
-		print "\n"
-	end
+  attr_accessor :mano_vardas
 
-	def Paveikslelis(bg, pix)
-		pic = Array.new
-		pic << Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-		pic << Array[0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0]
-		pic << Array[0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0]
-		pic << Array[0,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,1,0,0]
-		pic << Array[0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0]
-		pic << Array[0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]
-		pic << Array[0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]
-		pic << Array[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0]
-		pic << Array[0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0]
-		pic << Array[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0]
-		pic << Array[0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0]
-		pic << Array[0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0]
-		pic << Array[0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0]
-		pic << Array[0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0]
-		pic << Array[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-		pic.each do |line|
-			line.each do |simbol|
-				if simbol == 1
-					print "#{@mano_vardas[pix.ord]}"
-				else
-					print "#{@mano_vardas[bg.ord]}"
-				end
-			end
-			print "\n"
-		end
-	end
+  public
+
+  def initialize
+    @mano_vardas = []
+    (0...128).each do |i|
+      @mano_vardas[i] = i.chr
+    end
+  end
+
+  def vardo_spausdinimas
+    arr = [65, 110, 122, 101, 108, 105, 107, 97, 32, 84, 97, 107, 117, 110]
+    arr.each do |num|
+      print(@mano_vardas[num]).to_s
+    end
+    print "\n"
+  end
+
+  def paveikslelis(backgroung, pix)
+    pic = []
+    pic << Array[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    pic << Array[0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+    pic << Array[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+    pic << Array[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
+    pic << Array[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+    pic << Array[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+    pic << Array[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+    pic << Array[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
+    pic << Array[0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+    pic << Array[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+    pic << Array[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+    pic << Array[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    pic << Array[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    pic << Array[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    pic << Array[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    pic.each do |line|
+      line.each do |simbol|
+        if simbol == 1
+          print(@mano_vardas[pix.ord]).to_s
+        else
+          print(@mano_vardas[backgroung.ord]).to_s
+        end
+      end
+      print "\n"
+    end
+  end
 end
 
+# rubocop:enable Metrics/LineLength
+
+# rubocop:enable Metrics/MethodLength
+
+# rubocop:enable Metrics/AbcSize
+
 pic = Pic.new
-pic.VardoSpausdinimas
-pic.Paveikslelis(' ', '@')
+pic.vardo_spausdinimas
+pic.paveikslelis(' ', '@')
