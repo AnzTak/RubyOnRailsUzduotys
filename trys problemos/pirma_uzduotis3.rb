06036eae6b68a9779fe199fe3a4621cc5bb2510c
AnzTak <anzelika.liut@gmail.com>
2021-01-11 19:45:13 +0200

    trys problemos pataisymas

--- trys problemos/pirma_uzduotis3.rb
+++ trys problemos/pirma_uzduotis3.rb
@@ -1,3 +1,10 @@
-def sort_string(string)
-  # your code here
+# https://rubymonk.com/learning/books/1-ruby-primer/problems/5-string-length-finder
+# Find the length of strings in an array
+def length_finder(input_array)
+  arr = []
+  for i in 0...input_array.length
+    arr << input_array[i].length
+  end
+  arr
 end
+puts length_finder(['Ruby','Rails','C42'])
